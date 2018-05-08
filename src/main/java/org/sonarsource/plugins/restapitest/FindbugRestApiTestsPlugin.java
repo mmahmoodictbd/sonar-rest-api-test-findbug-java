b9a3db4cee5fb75e2f32f40f72e1d0c14e5f7f1e
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2018-05-08 22:31:12 +0200

    Added scratch plugin class.

---
+++ src/main/java/org/sonarsource/plugins/restapitest/FindbugRestApiTestsPlugin.java
@@ -0,0 +1,12 @@
+package org.sonarsource.plugins.restapitest;
+
+import org.sonar.api.Plugin;
+
+public class FindbugRestApiTestsPlugin implements Plugin {
+
+    @Override
+    public void define(Context context) {
+
+    }
+
+}
